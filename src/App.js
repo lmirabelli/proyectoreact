75bc1eec55fe992763f9b97497ab98dd7c5d7eea
lmirabelli <chuchenko1986@gmail.com>
2022-07-26 20:00:15 -0300

    03.counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import ProdCounter from './components/Contador/Contador';
 
 function App() {
+  const handleOnAdd = (quant) => {
+    console.log(`Se agregó: ${quant} productos`)
+  }
+
   return (
     <div className="App">
       <Navbar />
+      <ProdCounter stock={15} onAdd={handleOnAdd}/>
     </div>
   );
 }
---
+++ src/components/Contador/Contador.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './Contador.css'
+
+
+
+
+const ProdCounter = ({ stock, onAdd}) => {
+    const [counter, setCounter] = useState(0)
+
+    const increment = () => { counter < stock && setCounter(counter + 1) }
+
+    const decrement = () => { counter > 0 && setCounter(counter - 1) }
+
+    return (
+        <div className='contador'>
+            <h1>{counter}</h1>
+            <div className='botonera'>
+            <button onClick={decrement}> -1</button>
+            <button onClick={increment}> +1</button>
+            <button onClick={() => onAdd(counter)}>ADD</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default ProdCounter
